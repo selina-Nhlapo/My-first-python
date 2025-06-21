779fb96d3219e67f7da1dd2c8502c5105fa1d3d4
selina-Nhlapo <selinamaki95@gmail.com>
2025-06-21 20:56:29 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
 print("Hello, Selina! This is your first Python file on GitHub.")
+
+print("Hello, Selina! This is your first Python file on GitHub.")
+print("You're doing amazing. Keep learning and coding!")
